b09e968e94ae99538ebbc0277f8c3c62fbd483c9
Mickey <develop.agility@gmail.com>
2016-02-21 17:15:42 -0700

    almost taking orders

---
+++ public/js/order-form.js
@@ -0,0 +1,16 @@
+$(function(){
+
+  $('#size').children().each(function(x){
+
+    //console.log($(this).val());
+
+  })
+
+  $('#toppings').children().each(function(x){
+
+    //console.dir($(this).data('price'));
+
+  })
+
+
+});
